8639f52523adac7bbe3e86f6944101166ca7aab1
ichik <ichik@yandex.ru>
2010-02-12 20:10:59 +0300

    Fixing “whisper after inviting” inconvinience.

--- altinvite.lua
+++ altinvite.lua
@@ -2,6 +2,7 @@ function altinvite(self, linkData, link, button)
 	local linkType = string.sub(link, 3, 8)
         if _G.IsAltKeyDown() and linkType == "player" then
 		InviteUnit(string.sub(linkData,8,-6))
+		ChatFrameEditBox:ClearFocus()
 	end
 end
 for i= 1,7 do
