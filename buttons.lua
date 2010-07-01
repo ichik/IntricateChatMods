392ae6ce6bc05f884969283084320e05ef2e6f6f
ichik <ichik@yandex.ru>
2010-07-01 11:50:09 +0400

    Buttons

--- buttons.lua
+++ buttons.lua
@@ -37,19 +37,21 @@ local function OnClick(button)
 	UpdateBottomButton(button:GetParent())
 end
 
-for i= 1,7 do
-	local frame = _G['ChatFrame'..i]
+for i= 1,10 do
+	local frame = _G[format("%s%d%s", "ChatFrame", i]
 	frame:SetScript('OnMouseWheel', OnMouseWheel)
 	frame:HookScript('OnShow', UpdateBottomButton)
 	frame:EnableMouseWheel()
 	
-	local button = _G['ChatFrame'..i..'BottomButton']
+	local button = _G[format("%s%d%s", "ChatFrame", i, 'BottomButton']
 	button:HookScript('OnClick', OnClick)
 	button:SetAlpha(.6)
 	
-	Hide(_G['ChatFrame'..i..'UpButton'])
-	Hide(_G['ChatFrame'..i..'DownButton'])
+	Hide(_G[format("%s%d%s", "ChatFrame", i, 'UpButton'])
+	Hide(_G[format("%s%d%s", "ChatFrame", i, 'DownButton'])
 	UpdateBottomButton(frame)
+	frame:SetClampRectInsets(0,0,0,0) --Allow the chat frame to move to the end of the screen
 end
 
-ChatFrameMenuButton:Hide()
+ChatFrameMenuButton:Hide() --Hide the chat shortcut button for emotes/languages/etc
+FriendsMicroButton:Hide() --Hide the "Friends Online" count button
