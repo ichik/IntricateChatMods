e6695c7235ee1907ac3d499d1310a30169d4dbea
ichik <ichik@yandex.ru>
2010-11-12 20:27:39 +0300

    Fix scrolling

--- buttons.lua
+++ buttons.lua
@@ -18,6 +18,20 @@ local function OnClick(button)
 	UpdateBottomButton(button:GetParent():GetParent())
 end
 
+function FloatingChatFrame_OnMouseScroll(self, delta)
+	if delta < 0 then
+		if IsShiftKeyDown() then self:ScrollToBottom()
+		else
+			for i = 1, 3 do self:ScrollDown() end end
+	elseif delta > 0 then
+		if IsShiftKeyDown() then self:ScrollToTop()
+		else
+			for i = 1, 3 do self:ScrollUp()
+			end
+		end
+	end
+end
+
 hooksecurefunc('FloatingChatFrame_OnMouseScroll', UpdateBottomButton)
 for i= 1,10 do
 	local frame = _G['ChatFrame'..i]
